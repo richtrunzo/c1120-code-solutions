c53060ebf37c6e1a422f5b041a201b2c247600d0
Rtrunzo <richtrunzo@gmail.com>
2020-11-16 15:25:19 -0800

    Completed Javascript operators and expressions.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-console */
+var width = 10;
+var height = 5;
+var area = width * height;
+
+console.log("Value of area:", area);
+
+var bill = 10;
+var payment = 50;
+var change = payment - bill;
+
+console.log("Value of change:", change);
+console.log("Type of change:", typeof change);
+
+var quizes = 10;
+var midterm = 20;
+var final =  30;
+var grade = (quizes + midterm + final) / 3;
+
+console.log("value of grade:", grade);
+console.log("Type of grade:", typeof grade);
+
+var firstName = "Rich";
+var lastName = "Trunzo";
+var fullName = firstName + " " + lastName;
+
+console.log("Value of fullName:", fullName)
+console.log("Type of fullName:", typeof fullName);
+
+var pH = 6;
+var isAcidic = pH < 7;
+
+console.log("Value of isAcidic:", isAcidic);
+console.log("Type of isAcidic:", typeof isAcidic);
+
+var headCount = 12;
+var isSparta = 12 === 300;
+
+console.log("Value of isSparta:", isSparta);
+console.log("Type of isSparta:", typeof isSparta);
+
+var motto = fullName;
+var motto = motto + " is the GOAT";
+
+console.log("Value of motto:", motto);
+console.log("Type of motto:", typeof motto);
