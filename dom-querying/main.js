771b542dbd340f14fb0a89aa1b041dbb99ce690c
Rtrunzo <richtrunzo@gmail.com>
2020-11-23 11:07:50 -0800

    Completed dom querying.

---
+++ dom-querying/main.js
@@ -0,0 +1,26 @@
+console.log("hello, world");
+
+var h1 = document.querySelector("h1");
+console.log(h1);
+
+console.log(console.dir(h1));
+
+var explanation = document.querySelector("#explanation");
+
+console.log(explanation);
+
+console.dir(explanation);
+
+var hint = document.querySelector(".hint");
+
+console.log(hint);
+
+console.dir(hint);
+
+var p = document.querySelectorAll("p");
+
+console.log(p);
+
+var examplelink = document.querySelectorAll(".example-link");
+
+console.log(examplelink);
