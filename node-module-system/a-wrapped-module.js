de8f1475814f17ccb131ee567fc66871abb37131
richtrunzo <richtrunzo@gmail.com>
2020-12-31 23:45:50 +0000

    completed node module system.

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,14 @@
+console.log("Type of exports", typeof exports);
+console.log("Value of exports", exports);
+
+console.log("Type of require", typeof require);
+console.log("Value of require", require);
+
+console.log("Type of module", typeof module);
+console.log("Value of module", module);
+
+console.log("Type of _filename", typeof __filename);
+console.log("Value of _filename", __filename);
+
+console.log("Type of _dirname", typeof __dirname);
+console.log("Value of _dirname", __dirname);
