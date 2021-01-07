3f0d3b39125fe43aaf1739af873c74fc3163a90a
richtrunzo <richtrunzo@gmail.com>
2021-01-07 01:37:02 +0000

    Completed express delete.

---
+++ express-delete/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+
+const app = express();
+
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+app.get('/api/grades', (req, res) => {
+  let gradesArr = [];
+  for (let prop in grades) {
+    gradesArr.push(grades[prop]);
+  };
+  res.json(gradesArr);
+});
+
+app.delete('/api/grades/:id', (req, res) => {
+  if (req.params.id === "1") {
+    delete grades[12];
+  } else if (req.params.id === "2") {
+    delete grades[47];
+  } else if (req.params.id === "3") {
+    delete grades[273];
+  }
+  res.sendStatus(204);
+})
+
+
+app.listen(3000, () => {
+  console.log('Server is listening.')
+});
