b3cf59cb7190425737c3055955fb883bb5285ac0
richtrunzo <richtrunzo@gmail.com>
2021-01-08 22:34:17 +0000

    Updated espress notes API per feedback.

--- express-notes-api/index.js
+++ express-notes-api/index.js
@@ -8,27 +8,21 @@ app.use(express.json());
 
 app.get('/api/notes', (req, res, next) => {
 let getArray = [];
-let sendArray = [];
-for (let prop in data) {
-      getArray.push(data[prop]);
-    }; for (let prop in getArray[1]) {
-      sendArray.push(getArray[1][prop]);
+for (let prop in data['notes']) {
+      getArray.push(data['notes'][prop]);
     };
-  res.status(200).json(sendArray);
+  res.status(200).json(getArray);
   });
 
 
 app.get('/api/notes/:id', (req, res, next) => {
-  if(req.params.id < 0 || isNaN(req.params.id) === true) {
+  if (isNaN(req.params.id) === true || req.params.id < 0 ) {
     res.status(400).json({'error': 'id must be a positive integer'});
   };
-    let getArray = [];
-    for (let prop in data) {
-      getArray.push(data[prop]);
-  } if (getArray[1][req.params.id] === undefined) {
-    res.status(400).json({'error': 'id ' + req.params.id + ' is not found' });
+  if (data['notes'][req.params.id]) {
+    res.status(200).json(data['notes'][req.params.id]);
   } else {
-    res.status(200).json(getArray[1][req.params.id]);
+    res.status(400).json({ 'error': 'id ' + req.params.id + ' is not found' });
   };
 });
 
@@ -36,11 +30,11 @@ app.get('/api/notes/:id', (req, res, next) => {
 app.post('/api/notes', (req, res, next) => {
   if (req.body.content === undefined) {
     res.status(400).json({'error': 'content is a required field'});
-  } else if (req.body.content !== undefined) {
+  } else {
     let nextId = data['nextId'];
     req.body.id = nextId;
     data['notes'][nextId] = req.body;
-    data['nextId'] = req.body.id + 1;
+    data.nextId++;
     fs.writeFile('./data.json', JSON.stringify(data, null, 2), (err) => {
       if (err) {
         res.status(500).json({ 'error': 'an unexpected error occured' });
@@ -52,11 +46,11 @@ app.post('/api/notes', (req, res, next) => {
 
 
 app.delete('/api/notes/:id', (req, res, next) => {
-  if (req.params.id < 0 || isNaN(req.params.id) === true) {
+  if (isNaN(req.params.id) === true || req.params.id < 0) {
     res.status(400).json({'error': 'id must be a positive integer'});
   } else if (data['notes'][req.params.id] === undefined) {
     res.status(404).json({'error': 'cannot find note with id ' + req.params.id});
-  } else if (data['notes'][req.params.id] !== undefined) {
+  } else {
     delete data['notes'][req.params.id];
     fs.writeFile('./data.json', JSON.stringify(data, null, 2), (err) => {
       if (err) {
@@ -72,7 +66,7 @@ app.put('/api/notes/:id', (req, res, next) => {
     res.status(400).json({'error': 'id must be a positive integer and content is a required field'});
   } else if (data['notes'][req.params.id] === undefined) {
     res.status(404).json({ 'error': 'cannot find note with id ' + req.params.id });
-  } else if (data['notes'][req.params.id] !== undefined) {
+  } else {
     data['notes'][req.params.id]['content'] = req.body.content;
     fs.writeFile('./data.json', JSON.stringify(data, null, 2), (err) => {
       if (err) {
