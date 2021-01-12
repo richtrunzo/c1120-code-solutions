1ee8bdaccc2f05ad43a8681f46c9be728a74212c
richtrunzo <richtrunzo@gmail.com>
2021-01-12 23:18:34 +0000

    Updated sql-join.

--- sql-join/where-are-they.sql
+++ sql-join/where-are-they.sql
@@ -1,5 +1,7 @@
 select "addresses"."line1",
-       "cities"."name",
-       "addresses"."district"
+       "cities"."name" as "city",
+       "addresses"."district",
+       "countries"."name" as "country"
 from "addresses"
-join "cities" using ("cityId");
+join "cities" using ("cityId")
+join "countries" using ("countryId");
