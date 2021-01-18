b9ea3b02af0b631df9328260bbe01a8c2fad8216
richtrunzo <richtrunzo@gmail.com>
2021-01-18 18:36:06 +0000

    Completed react-elements

---
+++ react-elements/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const elem = React.createElement(
+  'h1',
+  [null],
+  [...'Hello, React!']
+)
+
+ReactDOM.render(elem, document.getElementById('root'));
