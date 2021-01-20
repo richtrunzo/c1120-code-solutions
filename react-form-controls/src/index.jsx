51df3838feb7e1f9234b9151a2a80758cc162a82
richtrunzo <richtrunzo@gmail.com>
2021-01-20 23:18:09 +0000

    Completed react-form-controls

---
+++ react-form-controls/src/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class NewsletterForm extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {value: " "};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(){
+    this.setState({value: event.target.value});
+  }
+  handleSubmit(){
+    event.preventDefault();
+    console.log('state:', this.state);
+  }
+  render(){
+    return <form onSubmit={this.handleSubmit}>
+            <label>
+              Email:
+              <input type="email" onChange={this.handleChange}></input>
+            </label>
+              <input type="submit" value="Sign Up"></input>
+           </form>
+  }
+}
+
+ReactDOM.render(<NewsletterForm />, document.querySelector('#root'));
