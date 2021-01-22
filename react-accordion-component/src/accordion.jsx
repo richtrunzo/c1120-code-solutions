3941c7bcdced5498894439c9624b6c2ab8e64628
richtrunzo <richtrunzo@gmail.com>
2021-01-22 05:12:15 +0000

    Updated react-accordion-component

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -4,100 +4,58 @@ import ReactDOM from 'react-dom';
 class Accordion extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {html: false, css: false, js: false};
-    this.jsOn = this.jsOn.bind(this);
-    this.jsRender = this.jsRender.bind(this);
-    this.cssOn = this.cssOn.bind(this);
-    this.htmlOn = this.htmlOn.bind(this);
-    this.htmlRender = this.htmlRender.bind(this);
-    this.cssRender = this.cssRender.bind(this);
-    this.start = this.start.bind(this);
-    this.startRender = this.startRender.bind(this);
+    this.state = {id: 0}
+    this.click = this.click.bind(this);
   }
-  jsOn() {
-    this.setState({ html: false, css: false, js: true});
-  }
-  cssOn(){
-    this.setState({html: false, css: true, js: false})
-  }
-  htmlOn(){
-    this.setState({html: true, css: false, js: false})
-  }
-  start() {
-    this.setState({ html: false, css: false, js: false })
-  }
-  htmlRender() {
-    return <div>
-            <div class="lang" onClick={this.start}>
-              <p>HyperText Markup Language</p>
-            </div>
-            <div class="text">
-              <p>{this.props.text[0].html}</p>
-            </div>
-            <div class="lang" onClick={this.cssOn}>
-              <p>Cascading Style Sheets</p>
-            </div>
-            <div class="lang" onClick={this.jsOn}>
-              <p>JavaScript</p>
-            </div>
-           </div>
-  }
-  cssRender() {
-    return <div>
-            <div class="lang" onClick={this.htmlOn}>
-              <p>HyperText Markup Language</p>
-            </div>
-            <div class="lang" onClick={this.start}>
-              <p>Cascading Style Sheets</p>
-            </div>
-            <div class="text">
-             <p>{this.props.text[0].css}</p>
-            </div>
-            <div class="lang" onClick={this.jsOn}>
-              <p>JavaScript</p>
-            </div>
-           </div>
-  }
-  jsRender() {
-    return <div>
-      <div class="lang" onClick={this.htmlOn}>
-        <p>HyperText Markup Language</p>
-      </div>
-      <div class="lang" onClick={this.cssOn}>
-        <p>Cascading Style Sheets</p>
-      </div>
-      <div class="lang" onClick={this.start}>
-        <p>JavaScript</p>
-      </div>
-      <div class="text">
-        <p>{this.props.text[0].js}</p>
-      </div>
-    </div>
-  }
-  startRender() {
-    return <div>
-            <div class="lang" onClick={this.htmlOn}>
-              <p>HyperText Markup Language</p>
-            </div>
-            <div class="lang" onClick={this.cssOn}>
-              <p>Cascading Style Sheets</p>
-            </div>
-            <div class="lang" onClick={this.jsOn}>
-              <p>JavaScript</p>
-            </div>
-            </div>
+  click() {
+    if (this.state.id === parseInt(event.target.id)) {
+      this.setState({id: 0})
+    } else {
+    this.setState({id: parseInt(event.target.id)})
+    }
+    console.log(this.state.id);
+    console.log(event.target.id);
   }
   render() {
-    if (this.state.html === true) {
-      return this.htmlRender();
-    } else if (this.state.css === true) {
-      return this.cssRender();
-    } else if (this.state.js === true) {
-      return this.jsRender();
-    } else {
-      return this.startRender();
-    }
-  }
-}
+    return (
+    this.props.contents.map(content => {
+      let showClass = 'hidden text';
+      if (this.state.id === content.id) {
+        showClass = 'active text';
+      }
+      return (
+          <div className="lang" onClick={this.click} id={content.id}>
+           <div onClick={this.click} id={content.id}>
+            <p onClick={this.click} id={content.id}>{content.title}</p>
+            </div>
+            <div className={showClass}>
+              <p>{content.content}</p>
+             </div>
+            </div>
+             )
+           })
+           )
+          }
+
+
+     }
+
 
 export default Accordion;
+
+
+// htmlRender() {
+//   return <div>
+//     <div class="lang" onClick={this.click}>
+//       <p>HyperText Markup Language</p>
+//     </div>
+//     <div class="text">
+//       <p>{this.props.text[0].html}</p>
+//     </div>
+//     <div class="lang" onClick={this.click}>
+//       <p>Cascading Style Sheets</p>
+//     </div>
+//     <div class="lang" onClick={this.jsOn}>
+//       <p>JavaScript</p>
+//     </div>
+//   </div>
--- react-accordion-component/src/index.jsx
+++ react-accordion-component/src/index.jsx
@@ -4,22 +4,32 @@ import Accordion from './accordion';
 
 const arr = [
   {
-    html: `Hypertext Markup Language(HTML) is the standard
+    title: `Hypertext Markup Language(HTML)`,
+    content: `Hypertext Markup Language(HTML) is the standard
             markup language for creating web pages and web
             applications.With Cascading Style Sheets(CSS) and
             JavaScript, it forms a triad of cornerstone
             technologies for the World Wide Web.`,
-    css: `Cascading Style Sheets(CSS) is a style sheet
+    id: 1
+  },
+  {
+    title: `Cascading Style Sheets (CSS)`,
+    content: `Cascading Style Sheets(CSS) is a style sheet
             language used for describing the presentation of a
             document written in a markup language like HTML.
             CSS is a cornerstone technology of the World Wide
             Web, alongside HTML and JavaScript`,
-    js: `JavaScript, often abbreviated as JS, is a high - level,
+    id: 2
+  },
+  {
+    title: `JavaScript`,
+    content: `JavaScript, often abbreviated as JS, is a high - level,
           interpreted programming language that conforms to
           the ECMAScript specification JavaScript has curly -
           bracket syntax, dynamic typing, prototype - based
-          obiect - orientation, and first - class functions.`
+          obiect - orientation, and first - class functions.`,
+    id: 3
   }
 ]
 
-ReactDOM.render(<Accordion text={arr}/>, document.querySelector('#root'));
+ReactDOM.render(<Accordion contents={arr}/>, document.querySelector('#root'));
